7f5d4bb5b8837ec91c0e41d7cbd62c1c0a810da2
aleksandarpredic <aleksandar.predic@gmail.com>
2020-01-14 22:48:11 +0100

    added autoloader for the theme, cleaned up theme files, setup webpack

---
+++ src/themes/predic-storefront/src/Traits/SingletonTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PredicStorefront\Traits;
+
+/**
+ * Trait SingletonTrait
+ *
+ * @package PredicWCPhoto\Traits
+ */
+trait SingletonTrait
+{
+    /**
+     * Class instance
+     * @var SingletonTrait
+     */
+    private static $instance;
+    /**
+     * Return Class instance
+     * @return $this
+     */
+    public static function getInstance()
+    {
+        if (!(self::$instance instanceof self)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
